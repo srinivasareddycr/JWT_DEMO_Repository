4e922dd85598386c8829f3073cbf37977d8164e2
unknown <cogjava3101@iiht.tech>
2022-08-19 11:25:38 +0530

    JWT USERAUTHENTICATION

---
+++ src/main/java/com/token/demo/service/SecurityConfiguration.java
@@ -0,0 +1,11 @@
+package com.token.demo.service;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+
+	
+
+}
---
+++ src/main/java/com/token/demo/service/UserService.java
@@ -0,0 +1,21 @@
+package com.token.demo.service;
+
+import java.util.ArrayList;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserService  implements UserDetailsService{
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return new User("admin","password", new ArrayList<>());
+	}
+
+	
+
+	
+
+}
