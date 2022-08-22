0e30673c0f91c8bbb99bb2341f39fb39e15d8bd7
unknown <cogjava3101@iiht.tech>
2022-08-22 09:38:11 +0530

    JWT TOKEN GENERATAION

--- src/main/java/com/token/demo/config/SecurityConfiguration.java
+++ src/main/java/com/token/demo/config/SecurityConfiguration.java
@@ -1,10 +1,15 @@
-package com.token.demo.service;
+package com.token.demo.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import com.token.demo.service.UserService;
 
 
 @Configuration
@@ -24,6 +29,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		auth.userDetailsService(userService);
 	}
 
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws  Exception
+	{
+		return super.authenticationManagerBean();
+	}
 	
+	
+	public  void  configure(HttpSecurity http) throws  Exception 	
+	{
+		http.csrf().disable().authorizeRequests().antMatchers("/authenticate").permitAll().anyRequest().authenticated();
+	}
 
 }
--- src/main/java/com/token/demo/controller/HomeController.java
+++ src/main/java/com/token/demo/controller/HomeController.java
@@ -1,10 +1,30 @@
 package com.token.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.token.demo.model.JwtRequest;
+import com.token.demo.model.JwtResponse;
+import com.token.demo.service.UserService;
+import com.token.demo.utility.JWTUtility;
 
 @RestController
 public class HomeController {
+	
+	@Autowired
+	private JWTUtility jwtUtility;
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	@Autowired
+	private UserService userService;
 
 	@GetMapping("/")
 	public  String home()
@@ -12,4 +32,23 @@ public class HomeController {
 		return " Welcome to JWT DEMO ";
 	}
 
+	
+	@PostMapping("/authenticate")
+	public JwtResponse authenticate(@RequestBody JwtRequest jwtRequest ) throws Exception
+	{
+		try {
+			authenticationManager.authenticate(
+
+					new  UsernamePasswordAuthenticationToken(jwtRequest.getUserName(), jwtRequest.getPassword()));
+		}catch(BadCredentialsException e)
+		{
+			 throw new Exception("invalid credentials" ,e);
+			
+			
+		}
+		final UserDetails userDetails =  userService.loadUserByUsername(jwtRequest.getUserName());
+		final String token= jwtUtility.generateToken(userDetails);
+		return  new JwtResponse(token);
+		}
+	
 }
